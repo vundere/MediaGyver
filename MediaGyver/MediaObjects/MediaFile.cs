e6199ca7ac229ea695c20c8cc1d01709e7430064
Olav Hobberstad <colddimension@gmail.com>
2024-05-22 22:11:42 +0200

    started adding media types

---
+++ MediaGyver/MediaObjects/MediaFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MediaGyver.MediaObjects
+{
+    internal abstract class MediaFile
+    {
+        public virtual required string FileDir { get; set } // Path to parent directory
+        public virtual required string FileName { get; set } // Filename, no path or extension
+        public virtual required string FileExtension { get; set } // Extension, obviously
+        public virtual string FullPath() {
+            string extensionLessPath =  Path.Join(FileDir, FileName);
+            return extensionLessPath += FileExtension;
+        }
+    }
+}
