5f09ea2ec0dd802ea271c657879ded5d7842d9a8
Olav Hobberstad <colddimension@gmail.com>
2024-05-22 20:50:38 +0200

    viewmodel add

---
+++ MediaGyver/Viewmodels/LandingWindow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGyver.Viewmodels
+{
+    public class LandingWindow : INotifyPropertyChanged
+    {
+
+        public LandingWindow()
+        {
+
+        }
+
+
+        #region Interface
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void RaisePropertyChange([CallerMemberName] string? propertyname = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+        #endregion
+    }
+}
