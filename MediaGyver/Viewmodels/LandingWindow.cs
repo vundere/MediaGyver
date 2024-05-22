07b83e95edf8ab1edf998f5d5eb398c46987b84d
Olav Hobberstad <colddimension@gmail.com>
2024-05-22 21:14:22 +0200

    dialog button

--- MediaGyver/Viewmodels/LandingWindow.cs
+++ MediaGyver/Viewmodels/LandingWindow.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.Input;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MediaGyver.Viewmodels
 {
     public class LandingWindow : INotifyPropertyChanged
     {
 
+        private ICommand _browseAddLibraryFolder;
+
         public LandingWindow()
         {
+            _browseAddLibraryFolder = new RelayCommand(BrowseAddLibraryFolder);
+        }
 
+        private void BrowseAddLibraryFolder()
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Select Folder...";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Do something with dialog.SelectedPath;
+                    
+                }
+            }
         }
 
 
