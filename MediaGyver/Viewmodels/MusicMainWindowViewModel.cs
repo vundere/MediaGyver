4af2c6c4d624f60d2a152e91b4f765565b7dbd27
Benktroger <github@benktroger.no>
2024-05-22 22:41:49 +0200

    Added Main Window for music and movies

---
+++ MediaGyver/Viewmodels/MusicMainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGyver.Viewmodels
+{
+	public class MusicMainWindowViewModel : INotifyPropertyChanged
+	{
+
+
+
+
+
+
+		#region Interface
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public void RaisePropertyChange([CallerMemberName] string? propertyname = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+		}
+		#endregion
+	}
+}
