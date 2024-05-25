f12885e27968cb0d28c1f83edc9661f17e2ff810
Benktroger <github@benktroger.no>
2024-05-25 13:16:27 +0200

    Added search button

--- MediaGyver/Viewmodels/MoviesMainWindowViewModel.cs
+++ MediaGyver/Viewmodels/MoviesMainWindowViewModel.cs
@@ -1,24 +1,48 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MediaGyver.Viewmodels
 {
     public class MoviesMainWindowViewModel : INotifyPropertyChanged
     {
 
+		private ICommand _searchButtonPressCommand;
+
+        public MoviesMainWindowViewModel()
+        {
+			_searchButtonPressCommand = new RelayCommand(SearchButtonClicked);
+		}
+
+		public ICommand SearchButtonPressCommand => _searchButtonPressCommand;
+
+
+		private void SearchButtonClicked()
+		{
+			
+
+		}
 
 
 
 
 
 
-        #region Interface
-        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+
+
+
+
+
+		#region Interface
+		public event PropertyChangedEventHandler? PropertyChanged;
 
         public void RaisePropertyChange([CallerMemberName] string? propertyname = null)
         {
